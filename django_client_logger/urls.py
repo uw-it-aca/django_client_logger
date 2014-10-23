5cf815a7bc6063f3f2f952221d6905c2e1e15284
Stephen De Vight <devights@gmail.com>
2014-10-22 17:33:25 -0700

    fixing urls

--- django_client_logger/urls.py
+++ django_client_logger/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import patterns, include, url
 from django_client_logger.views.log_receiver import LogReceiver
 
 urlpatterns = patterns('',
-    url(r'log', LogReceiver().run),
-    url(r'view', 'django_client_logger.views.page.index')
+    url(r'log', LogReceiver().run)
 )
