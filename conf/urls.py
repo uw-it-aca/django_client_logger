c05a369254e1c8321e54bd5feabf7fe4e815b980
Fang Lin <fanglin@uw.edu>
2024-01-30 13:41:55 -0800

    upgrade django, python (#11)
    Upgrade django, python,action, copyright year

--- conf/urls.py
+++ conf/urls.py
@@ -1,6 +1,9 @@
-from django.conf.urls import include, url
+# Copyright 2024 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
+from django.urls import include, re_path
 
 
 urlpatterns = [
-    url(r'^', include('django_client_logger.urls')),
+    re_path(r'^', include('django_client_logger.urls')),
 ]
